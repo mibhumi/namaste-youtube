afbc4ab05784840d0419181e82f1270d12cadbc9
Bhumi Patel <bhumipatel1196@gmail.com>
2023-02-28 20:24:04 -0500

    basuc structure of application

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import MainContainer from './MainContainer'
 import Sidebar from './Sidebar'
 
 const Body = () => {
   return (
     <div>
         <Sidebar></Sidebar>
+        <MainContainer></MainContainer>
     </div>
   )
 }
