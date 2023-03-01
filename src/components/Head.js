9b05646f0598893c4a5c8614a136f3851f3bb1b3
Bhumi Patel <bhumipatel1196@gmail.com>
2023-02-28 20:49:08 -0500

    add header

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,33 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <div>Head</div>
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+        <div className="flex col-span-1">
+            <img
+                className='h-8'
+                alt="menu"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbOFYlVrU65tBnWYP5bmEKpaCQbK7FMBMQJtRV7TJJug&s"
+            />
+            <img
+                className='h-8 mx-2'
+                alt="logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+            />
+        </div>
+        <div className="col-span-10 px-10">
+            <input className="border-gray-400 w-1/2 border p-2 rounded-l-full" type="text" />
+            <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+                🔍
+            </button>
+        </div>
+        <div>
+            <img 
+                className='h-8'
+                alt="user"
+                src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"     
+            />
+        </div>
+    </div>
   )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="p-5 shadow-lg col-span-1">
+        <h1>Subscriptions</h1>
+        <ul>
+            <li>Music</li>
+            <li>Sports</li>
+            <li>Gaming</li>
+            <li>Movies</li>
+        </ul>
+    </div>
   )
 }
 
